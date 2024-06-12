863909ea36826527c0cf27d704991602332feec0
Rafael Zuquim <rzuquim@gmail.com>
2024-06-12 08:45:53 -0300

    feat: bootstrap script (to be placed in a public gist) cloning the dotfiles (this) repo

---
+++ README.md
@@ -0,0 +1,35 @@
+
+[<img align="right" src="http://www.inoa.com.br/resources/logo_email_sig.png" width="100" />](http://inoa.com.br/)
+
+# inoa dotfiles ⚛
+
+Automation to bootstrap a [POP_OS!](https://pop.system76.com/) inoa dev environment.
+
+# Running 
+
+Open a terminal, paste this and hope for the best.
+
+```
+. <(curl -sL https://inoa.com.br/dev_boostrap.sh)
+```
+
+# Contributing
+
+We prefer [TBD](https://trunkbaseddevelopment.com/) to preserve a linear history. So please, always rebase before MR.
+
+The commit messages should follow the
+[ Angular commit conventions](https://github.com/angular/angular.js/blob/master/DEVELOPERS.md#-git-commit-guidelines).
+You might find useful to use the [commitizen](https://github.com/commitizen/cz-cli/) tool to enforce the correct format.
+
+If your MR is linked to a card on [trello](https://trello.com/), we encourage you to add the link to the car on the
+`longer description` field of your commit message.
+
+ 1. **Branch** over the `latest` branch
+ 2. **Commit** your changes to your own branch
+ 3. **Merge request** setting yourself as the responsible and choosing the appropriate reviewer
+ 4. **Discuss and change** the code so the reviewer can close the discussions
+ 5. **Clean up** the commits and squash them to a minimal and meaningful changeset
+ 7. Make sure the CHANGELOG was updated and the **version** bumped
+ 8. **Merge** into `latest`
+
+NOTE: Please, keep your commits small and let the reviewer choose to squash them or not.
---
+++ bootstrap.gist.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# DOTFILES_REPO=git@github.com:inoa/dotfiles.git
+DOTFILES_REPO=git@github.com:inoa-rzuquim/dotfiles.git
+GIST_REPO=https://inoa.com.br/dev_boostrap.sh
+
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN="\e[38;2;0;0;255m"
+INDIGO="\e[38;2;75;0;130m"
+VIOLET="\e[38;2;238;130;238m"
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+if [ ! -t 0 ]; then
+    echo "Please run this script interactively."
+    echo ". <(curl -sL $GIST_REPO)"
+    exit 1
+fi
+
+echo -e "${CYAN}                @@${NC}" 
+echo -e "${CYAN}                @@@${NC}" 
+echo -e "${CYAN}                @@@${NC}" 
+echo -e "${CYAN}                @@@@${NC}" 
+echo -e "${CYAN}       @@@      @@@@${NC}" 
+echo -e "${CYAN}        @@@     @@@@${NC}" 
+echo -e "${CYAN}         @@@    @@@@${NC}" 
+echo -e "${CYAN}         @@@@   @@@@${NC}" 
+echo -e "${CYAN}          @@@@  @@@@${NC}" 
+echo -e "${CYAN}   @@@@@    @@@@ @@@@${NC}      !                     @@@" 
+echo -e "${CYAN}      @@@@  @@@@ @@@@${NC}      !  @@ @@@      @@   @@@  @@@@      @@@" 
+echo -e "${CYAN}        @@@@  @@@ @@@${NC}      !  @@ @@ @@    @@  @@       @@    @ @@" 
+echo -e "${CYAN}          @@@@ @@@ @@${NC}      !  @@ @@  @@   @@ @@         @@  @@  @@" 
+echo -e "${CYAN}   @@@@@@   @@@@${NC}           !  @@ @@   @@  @@ @@         @@ @@    @@" 
+echo -e "${CYAN}    @@@@@@@@@@@@       @${NC}   !  @@ @@    @@ @@ @@         @@ @      @" 
+echo -e "${CYAN}          @@@@@@    @@${NC}     !  @@ @@     @@@@ @@@       @@ @@      @@" 
+echo -e "${CYAN}      @@@@@@@@@@@   @@@${NC}    !  @@ @@      @@@   @@     @@ @@        @@" 
+echo -e "${CYAN}    @@@@@@@  @@@@@@@@@ @@${NC}  !  @@ @@       @@    @@@@@@@  @          @" 
+echo -e "${CYAN}          @@@@ @@ @ @ @@${NC}" 
+echo -e "${CYAN}        @@   @@ @@ @${NC}" 
+echo -e "${CYAN}            @@  @   @${NC}" 
+echo -e "${CYAN}            @${NC}" 
+
+echo "Bootstraping dev env"
+read -p "Please enter your inoa email: " INOA_EMAIL
+echo
+
+CLONE_SUCCESSFUL=false
+
+mkdir -p ~/.inoa/
+cd ~/.inoa/
+
+while [ "$CLONE_SUCCESSFUL" == false ]; do
+    echo "Trying to clone $DOTFILES_REPO"
+    git clone $DOTFILES_REPO
+
+    if [ $? -eq 0 ]; then
+        break
+    fi
+
+    echo -e "${RED}!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!${NC}"
+    echo -e "${RED}Could not clone dotfiles repository!${NC}"
+    echo -e "${RED}!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!${NC}"
+
+    # Check if an SSH key already exists
+    if [ ! -f ~/.ssh/id_ed25519 ]; then
+        echo -e "${CYAN}No SSH key found. Generating a new SSH key...${NC}"
+        ssh-keygen -t ed25519 -C "$INOA_EMAIL"
+        echo -e "${GREEN}SSH key generated and added to the ssh-agent.${NC}"
+        echo
+    fi
+
+    echo -e "${YELLOW}Copy your public key to your GitHub account:${NC}"
+    echo -e "${YELLOW}https://github.com/settings/ssh/new${NC}"
+    echo
+
+    cat ~/.ssh/id_ed25519.pub
+    read -n 1 -s -r -p "After adding the key on GitHub, press any key to try again..."
+done
+
+echo -e "${GREEN}SUCESSO{NC}"
+
