8477acfdd2ce7080599090a69905c4b28a914df6
Rafael Zuquim <rzuquim@gmail.com>
2024-06-14 11:19:26 -0300

    asd

--- basic/security.sh
+++ basic/security.sh
@@ -12,10 +12,12 @@ response=${response:-Y}
 if [[ $response =~ ^[Yy]$ ]]; then
     VPN_PWD_FILE=/etc/secrets/vpn-pwd
     if [ ! -f $VPN_PWD_FILE ]; then
-        read -p "Please enter your VPN password: " INOA_VPN_PASSWORD
-        echo "$INOA_EMAIL" > $VPN_PWD_FILE
-        echo "$INOA_VPN_PASSWORD" >> $VPN_PWD_FILE
+        read -s "Please enter your VPN password: " INOA_VPN_PASSWORD
+        echo
+        sudo echo "$INOA_EMAIL" > $VPN_PWD_FILE
+        sudo echo "$INOA_VPN_PASSWORD" >> $VPN_PWD_FILE
     fi
+
     download_full_ovpn
 else
     echo -e "${RED}Won't apply vpn configuration!${NC}"
