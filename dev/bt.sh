6ace8107c1ab3c0934bbb58328814b9a197f9fc1
Renato Sampaio <rsampaio@inoa.com.br>
2024-06-22 15:54:58 +0000

    fix: bt.sh docker path

--- dev/bt.sh
+++ dev/bt.sh
@@ -4,6 +4,6 @@ echo -e "${VIOLET}* Running RabbitMQ and SQL Server${NC}"
 echo
 
 docker compose -p bt-dev down
-docker compose -f ~/.inoa/dev/bt.deps.docker-compose -p bt-dev up -d
+docker compose -f ~/.inoa/dotfiles/dev/bt.deps.docker-compose.yml -p bt-dev up -d
 
 # TODO: health-check
